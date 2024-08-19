4aebca5c4876085f7c61057439c077b973b64129
WenYiHuanooo <162790849+WenYiHuanooo@users.noreply.github.com>
2024-08-19 23:24:52 +0800

    Update test_nativecpu_transpose.cc

--- test/kernels/nativecpu/test_nativecpu_transpose.cc
+++ test/kernels/nativecpu/test_nativecpu_transpose.cc
@@ -8,20 +8,22 @@
 namespace infini {
 
 TEST(Transpose, NativeCpu) {
-    Runtime runtime = NativeCpuRuntimeObj::getInstance();
-    Graph g = make_ref<GraphObj>(runtime);
+  Runtime runtime = NativeCpuRuntimeObj::getInstance();
+  Graph g = make_ref<GraphObj>(runtime);
 
-    Shape permute = {0, 2, 1, 3};
-    auto input = g->addTensor({1, 2, 3, 4}, DataType::Float32);
-    auto op = g->addOp<TransposeObj>(input, nullptr, permute);
-    g->dataMalloc();
-    input->setData(IncrementalGenerator());
-
-    runtime->run(g);
-
-    EXPECT_TRUE(op->getOutput(0)->equalData(vector<float>{0, 1, 2, 3, 12, 13, 14, 15,
-                                                          4, 5, 6, 7, 16, 17, 18, 19,
-                                                          8, 9, 10, 11, 20, 21, 22, 23}));
+  Shape permute = {0, 2, 1, 3};
+  auto input = g->addTensor({1, 2, 3, 4}, DataType::Float32);
+  auto op = g->addOp<TransposeObj>(input, nullptr, permute);
+  g->dataMalloc();
+  input->setData(IncrementalGenerator());
+//   std::cout << "before run\n";
+//   input->printData();
+  runtime->run(g);
+//   std::cout << "after run\n";
+//   op->getOutput(0)->printData();
+  EXPECT_TRUE(op->getOutput(0)->equalData(
+      vector<float>{0,  1,  2,  3,  12, 13, 14, 15, 4,  5,  6,  7,
+                    16, 17, 18, 19, 8,  9,  10, 11, 20, 21, 22, 23}));
 }
 
 } // namespace infini

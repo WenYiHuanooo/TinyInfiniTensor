858f3b685c14eb791cb5471c38e2321893157e08
WenYiHuanooo <162790849+WenYiHuanooo@users.noreply.github.com>
2024-08-19 23:20:31 +0800

    Update concat.cc

--- src/operators/concat.cc
+++ src/operators/concat.cc
@@ -1,5 +1,7 @@
 #include "operators/concat.h"
+#include "core/tensor.h"
 #include "utils/operator_utils.h"
+#include <cstddef>
 
 namespace infini {
 ConcatObj::ConcatObj(GraphObj *graph, TensorVec inputs, Tensor output, int _dim)
@@ -11,13 +13,13 @@ ConcatObj::ConcatObj(GraphObj *graph, TensorVec inputs, Tensor output, int _dim)
 
 optional<vector<Shape>> ConcatObj::inferShape(const TensorVec &inputs) {
     Shape dims = inputs[0]->getDims();
-    auto rank = inputs[0]->getRank();
-
     // =================================== 作业 ===================================
     // TODO：修改 dims，返回正确的 concat 后的 shape
     // REF: https://onnx.ai/onnx/operators/onnx__Concat.html#concat-13
     // =================================== 作业 ===================================
-
+    for (std::size_t i = 1; i < inputs.size(); ++ i) {
+        dims[dim] += inputs[i]->getDims()[dim];
+    }
     return {{dims}};
 }
 
